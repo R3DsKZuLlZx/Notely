2088af0cf41c43b3b4137707ef792bbdb3bb6ece
R3DsKZuLlZx <r3dskzullzx@gmail.com>
2023-11-16 19:20:02 +0000

    📓 Add PWA support.

--- src/Notely.WebUI/projects/web-app/src/app/app.config.ts
+++ src/Notely.WebUI/projects/web-app/src/app/app.config.ts
@@ -1,12 +1,17 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { HttpClientModule } from '@angular/common/http';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     importProvidersFrom(HttpClientModule),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })
+]
 };
